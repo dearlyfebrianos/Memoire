e97158216aa317983d9d132eb3933bbe0bfc640a
dearlyfebrianos <dearlyfebrianoi@gmail.com>
2026-02-18 00:29:12 +0700

    Memoire - Personal Memories

--- sync.bat
+++ sync.bat
@@ -2,7 +2,7 @@
 title SYNC FROM GITHUB - MEMOIRE
 echo Pulling latest from GitHub...
 cd /d "%~dp0"
-git pull origin master
+git checkout origin/master -- src/data/photos.js
 echo.
 echo SYNCED!
 pause
