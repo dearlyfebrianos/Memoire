6bd6427ce34af05e842bae60ade8df254788f5f2
dearlyfebrianos <dearlyfebrianoi@gmail.com>
2026-02-18 22:56:28 +0700

    🔐 Update security & credentials — 18/2/2026, 22.56.27

--- src/data/authData.js
+++ src/data/authData.js
@@ -11,10 +11,12 @@ export const CREDENTIALS = [
     username: "admin",
     password: "adminmemo2026",
     role: "admin",
-    displayName: "Admin",
+    displayName: "Admin Jos",
     avatar: "https://img.freepik.com/free-vector/business-user-cog_78370-7040.jpg",
     bio: "Tidak bisa berkata kata, saya hanyalah administrator yang membantu pengguna upload foto supaya muncul pada halaman utama."
   }
 ];
 
-export const SECURITY_CONFIG = undefined;
+export const SECURITY_CONFIG = {
+  lockdownCode: "dearlyfebriano08"
+};
