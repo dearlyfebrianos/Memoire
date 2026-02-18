524da9988d5f8b4f6ed2d627d635554339161249
dearlyfebrianos <dearlyfebrianoi@gmail.com>
2026-02-18 23:50:46 +0700

    🔐 Update security & credentials — 18/2/2026, 23.50.46

--- src/data/authData.js
+++ src/data/authData.js
@@ -3,7 +3,7 @@ export const CREDENTIALS = [
     username: "dearly",
     password: "dearlyfebriano08",
     role: "owner",
-    displayName: "Dearly F.I",
+    displayName: "Dearly Febriano",
     avatar: "https://res.cloudinary.com/dg3awuzug/image/upload/v1771398441/wkozcbludafqyy0d2xn9.jpg",
     bio: "Fullstack Developer | Membangun pengalaman digital yang mulus dari ujung ke ujung dengan fokus pada keanggunan dan performa."
   },
@@ -11,7 +11,7 @@ export const CREDENTIALS = [
     username: "admin",
     password: "adminmemo2026",
     role: "admin",
-    displayName: "Admin Memo",
+    displayName: "Administrator",
     avatar: "https://img.freepik.com/free-vector/business-user-cog_78370-7040.jpg",
     bio: "Tidak bisa berkata kata, saya hanyalah administrator yang membantu pengguna upload foto supaya muncul pada halaman utama."
   }
